dfcb0c092461bae0874558a85c35ac83d2dd0eba
Benjamin <benuserxxx@gmail.com>
2025-04-11 19:48:12 -0500

    cos2025011

--- config/cors.php
+++ config/cors.php
@@ -18,8 +18,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://conectservfront.vercel.app', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
